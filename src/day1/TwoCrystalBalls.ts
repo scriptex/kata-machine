3403e76729522ebfedd0add408960fc6dc87b4d0
Atanas Atanasov <scriptex.bg@gmail.com>
2022-11-08 16:44:11 +0200

    Implement two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,1 +1,21 @@
-export default function two_crystal_balls(breaks: boolean[]): number {}
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const amount = Math.floor(Math.sqrt(breaks.length));
+
+    let i = amount;
+
+    for (; i < breaks.length; i += amount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= amount;
+
+    for (let j = 0; j < amount && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
+}
